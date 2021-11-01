742a6d9e87617b89cec3f2fe3276c0e255c1939a
zhangjing <978582067@qq.com>
2021-11-01 22:13:23 +0800

    commi

---
+++ c/c_main.c
@@ -0,0 +1,10 @@
+//
+// Created by 张晶 on 2021/11/1.
+//
+
+#include "stdio.h"
+#include "c_main.h"
+
+int main(){
+    printf("hello world");
+}
---
+++ c/c_main.h
@@ -0,0 +1,8 @@
+//
+// Created by 张晶 on 2021/11/1.
+//
+
+#ifndef C_LEARN_C_MAIN_H
+#define C_LEARN_C_MAIN_H
+
+#endif //C_LEARN_C_MAIN_H
