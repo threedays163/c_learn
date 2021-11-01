30a5dcbcdf46830f6a407aaa4b185b97fd7c2d10
zhangjing <978582067@qq.com>
2021-11-01 22:00:54 +0800

    [A]init

---
+++ cmake-build-debug/CMakeFiles/c_learn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_learn.dir/main.cpp.o"
+  "c_learn"
+  "c_learn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
